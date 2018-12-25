b583228609171bb8441b71b7f00c09223177de57
Anthony <amk1997@hotmail.se>
2018-12-25 17:54:24 +0100

    Fix build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	mode: "development",
@@ -6,5 +8,16 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "out"),
 		filename: "main.js"
-	}
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{
+				from: "src/main.css"
+			}
+		]),
+		new HtmlWebpackPlugin({
+			title: "BoidsWallpaper",
+			template: "src/template.html"
+		})
+	]
 };
